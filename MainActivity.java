a754017712487ccfe02c75ba6fa7527df9da0f5c
Chaitanyasuma <43496656+Chaitanyasuma@users.noreply.github.com>
2019-11-21 19:39:32 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,56 +1,118 @@
-//working on the db functions
+package com.example.game;
 
-package com.example.android.game;
+/*import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner s1;
+    Button b1;
+    int flag = 0;
+    MediaPlayer mobj;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mobj = new MediaPlayer();
+
+        s1=(Spinner)findViewById(R.id.spinner1);
+        b1 = (Button) findViewById(R.id.button1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String string = String.valueOf(s1.getSelectedItem());
+                if(string.equalsIgnoreCase("Mellow")) {
+                    playSong1();
+                }
+
+                else if(string.equalsIgnoreCase("Ambience")) {
+                    playSong2();;
+                }
+
+                else {
+                    playSong3();
+                }
+            }
+        });
+    }
+
+    public void playSong1() {
+        mobj = MediaPlayer.create(this, R.raw.song);
+        mobj.start();
+    }
+
+    public void playSong2() {
+        mobj = MediaPlayer.create(this, R.raw.ambience);
+        mobj.start();
+
+    }
+
+    public void playSong3() {
+        mobj = MediaPlayer.create(this, R.raw.rock);
+        mobj.start();
+
+    }
+
+    public void playGame(View view) {
+        Intent obj = new Intent(this, Grid.class);
+        startActivity(obj);
+    }
+
+    public void rulesopen(View view) {
+        Intent obj2 = new Intent(this, Rules.class);
+        startActivity(obj2);
+    }
+
+}*/
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.WindowManager;
 
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText nameip;
-    TextView scores;
-    DBHandler db;
-    Intent i;
-    
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
+    //After completion of 2000 ms, the next activity will get started.
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
         setContentView(R.layout.activity_main);
+        //this will bind your MainActivity.class file with activity_main.
 
-        db= new DBHandler(this);
-        nameip = (EditText) findViewById(R.id.newPlayerET);
-        scores= (TextView) findViewById(R.id.highScoresTV);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Home.class);
+                //Intent is used to switch from one activity to another.
 
-        displayHighScores();
-        i= getIntent();
-    }
+                startActivity(i);
+                //invoke the SecondActivity.
 
-    public void playGame(View view)
-    {
-        Intent intent=new Intent(this, Grid.class);
-        startActivity(intent);
-    }
-
-    public void addPlayerToDB(View view)
-    {
-        String name =  nameip.getText().toString();
-        db.addPlayer(new PlayerDB(id, name, 0 ));
-    }
-
-    public void displayHighScores()
-    {
-        List<PlayerDB> players = db.getPlayers();
-        for (PlayerDB i : players) {
-            String s = i.getID() + "  " + i.getName() + "   " + i.getScore();
-            scores.setText(s);
-        }
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
-
